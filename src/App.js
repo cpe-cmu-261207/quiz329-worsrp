705c174d6cbc882e343e98e8316a5ed147239342
worsrp <59278366+worsrp@users.noreply.github.com>
2021-02-09 14:16:21 +0700

    i have nothing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { useState, useEffect } from "react";
 
 function App() {
   //add useState for all state variables
-
+  const [task, setTask] = useState({});
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState(0);
   //load locationStorage
   useEffect(() => {
     const items = localStorage.getItem("items");
     // ...
   }, []);
 
+  const addTask = (e) => {
+    setTask([...task, name, gender, age]);
+    localStorage.setItem("items", JSON.stringify(task));
+  };
   return (
     <div className="card" style={{ width: 400 }}>
       <div className="card-content">
@@ -21,13 +28,21 @@ function App() {
             type="text"
             placeholder="e.q John Smith"
             //update related state based on event
+            onChange={(e) => setName(e.currentTarget.value)}
           ></input>
         </div>
 
         <div className="field">
           <label className="label">Gender</label>
           <select className="input" type="text" placeholder="Please select ..">
-            <option value="" disabled selected hidden>
+            <option
+              value=""
+              disabled
+              selected
+              hidden
+              onChange={(e) => setGender(e.currentTarget.value)}
+              defaultValue={gender.value}
+            >
               -- Select Gender --
             </option>
             <option>Male</option>
@@ -37,10 +52,17 @@ function App() {
 
         <div className="field">
           <label className="label">Age</label>
-          <input className="input" type="number" placeholder="e.q 30"></input>
+          <input
+            className="input"
+            type="number"
+            placeholder="e.q 30"
+            onChange={(e) => setAge(e.currentTarget.value)}
+          ></input>
         </div>
 
-        <button className="button is-primary is-fullwidth">Submit</button>
+        <button className="button is-primary is-fullwidth" onClick={addTask}>
+          Submit
+        </button>
 
         <div className="mb-4"></div>
 
@@ -48,7 +70,7 @@ function App() {
         <p className="is-4 title has-text-centered">Person List</p>
         {/* sample table */}
         <ItemTable name={"Bob"} gender={"Male"} age={"50"} />
-        <p>Your name and code here</p>
+        <p>Sirapop Para 620610815</p>
       </div>
     </div>
   );
